c54c932b04f31fe8c17096c5aa5f36c02a40f978
fredd <freddypcarrion@gmail.com>
2022-10-05 09:55:26 -0500

    First commit of the app.

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Welcome to software for Petroleum Engineering")
+st.write("This app is going to be used as the kickoff of this lecture")
+st.write("This is the second class of the lecture PETG1029")
