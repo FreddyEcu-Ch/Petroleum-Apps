46ed1fba9e4815a1caebfb1a98314303a379dc52
fredd <freddypcarrion@gmail.com>
2022-10-25 09:44:59 -0500

    Test xlwings update

--- poes/test_xlwings.py
+++ poes/test_xlwings.py
@@ -1,5 +1,6 @@
 import xlwings as xw
 import numpy as np
+import pandas as pd
 
 #%% Create a workbook
 wb = xw.Book()
@@ -15,3 +16,19 @@ print(values)
 
 #%% Create a numpy array from Python
 sheet["G6"].options(np.array, transpose=True).value = np.array([1, 2, 3, 4, 5])
+
+#%% Call a numpy array from MS Excel to Python
+values = sheet["G6:K6"].value
+print(values)
+
+#%% Create a DataFrame from Python to MS Excel
+sheet["D13"].options(pd.DataFrame, expand="table", index=False).value = pd.DataFrame(
+    {
+        "Oilfield": np.array(["Auca", "Sacha", "Sushufindi"]),
+        "Wells": np.array(["Auc-001", "Sa-006", "Shu-009"]),
+        "Oil_production (bpd)": np.array([300, 1000, 1500]),
+    }
+)
+
+#%% Call a DataFrame form MS Excel to Python
+df = sheet["D13"].options(pd.DataFrame, expand="table", index=False).value
